27bdcb13953da5e1d3347f47b722546201c33d50
BrandonSRT <brandon.se2009@gmail.com>
2021-02-08 02:17:15 -0600

    ds

--- Assets/NumeroMagico.cs
+++ Assets/NumeroMagico.cs
@@ -27,7 +27,7 @@ public class NumeroMagico : MonoBehaviour
     }
     //esto es para probar el tema de los conflictos
     //sdfhsf
-    //kdsjfalsjdf
+    //kdsjfalsjdfsd
 
     // Update is called once per frame
     void Update()
